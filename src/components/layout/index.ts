fa121288d8f5c0df890a7192d39cc85481afe5eb
SeHIgh <sehi0119@naver.com>
2025-08-17 03:59:53 +0900

    fix(layout): AppSidebar import 오류 수정
    - AppSidebar를 named export로 올바르게 수정
    - RootLayout에서 컴포넌트 undefined 오류 해결

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,6 +1,6 @@
 // * Layout 컴포넌트들 export
 export { default as Header } from './header';
 export { default as MainLayout } from './main';
-export { default as AppSidebar } from './sidebar';
+export { AppSidebar } from './sidebar';
 export { LayoutProvider, useLayout } from './LayoutContext';
 export { PageHeader, usePageHeader } from './PageHeader';
